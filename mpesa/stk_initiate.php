81082f1eb8a9955c8b9e4d1d840fa30c6a5c684f
Justus-Otundo <justusotundo86@gmail.com>
2024-03-07 18:13:19 +0300

    update

--- mpesa/stk_initiate.php
+++ mpesa/stk_initiate.php
@@ -3,7 +3,7 @@
 include 'accessToken.php';
 date_default_timezone_set('Africa/Nairobi');
 $processrequestUrl = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
-$callbackurl = 'https://cb54-102-217-157-219.ngrok-free.app/HostelManagement/mpesa/callback_url.php';
+$callbackurl = 'https://a96b-102-217-157-219.ngrok-free.app//HostelManagement/mpesa/callback_url.php';
 $passkey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919";
 $BusinessShortCode = '174379';
 $Timestamp = date('YmdHis');
